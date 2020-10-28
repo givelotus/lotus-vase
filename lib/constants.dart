c783adff7df9903de15362050eb69f7a6ce2a185
Harry Barber <harrybarber@protonmail.com>
2020-10-28 02:04:15 +0000

    Connect electrum to wallet

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const cardElevation = 6.0;
 const cardPadding = EdgeInsets.all(12.0);
-const electrumUrl = 'ws://electrum.bitcoincash.org';
+const electrumUrl = 'ws://51.79.81.158:50003';
--- lib/wallet.dart
+++ lib/wallet.dart
@@ -1,8 +1,29 @@
+import 'constants.dart';
+import 'electrum/rpc.dart';
+
 class Wallet {
   Wallet(String walletPath);
 
+  int _balance;
+  ElectrumClient client = ElectrumClient();
+
+  Future<void> refreshWallet() async {
+    const exampleScriptHash =
+        '8b01df4e368ea28f8dc0423bcf7a4923e3a12d307c875e47a0cfbf90b5c39161';
+    final response =
+        await client.blockchainScripthashGetBalance(exampleScriptHash);
+
+    final totalBalance = response.confirmed + response.unconfirmed;
+    _balance = totalBalance;
+  }
+
+  Future<void> initWallet() async {
+    await client.connect(Uri.parse(electrumUrl));
+    await refreshWallet();
+  }
+
   int balanceSatoshis() {
-    return 1000;
+    return _balance;
   }
 
   String getAddress() {
