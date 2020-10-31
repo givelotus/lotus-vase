b9c69e3b4848d1b981bea1c92c7629a693ce0629
Harry Barber <harrybarber@protonmail.com>
2020-10-31 14:06:58 +0000

    Add basic UTXO storage

---
+++ lib/wallet/utxos.dart
@@ -0,0 +1,50 @@
+import 'dart:collection';
+
+import 'package:cashew/bitcoincash/bitcoincash.dart';
+
+class UtxoStorage {
+  UtxoStorage(Iterable<TransactionOutput> utxos) {
+    final zipped = {for (final utxo in utxos) utxo.satoshis: utxo};
+    utxoPool = SplayTreeMap.from(zipped);
+  }
+  SplayTreeMap<BigInt, TransactionOutput> utxoPool;
+
+  /// Gets the smallest output more than a specific amount.
+  TransactionOutput smallestAbove(BigInt amount) {
+    final key = utxoPool.firstKeyAfter(amount);
+    return utxoPool[key];
+  }
+
+  /// Gets the largest output below a specific amount.
+  TransactionOutput largestBelow(BigInt amount) {
+    final key = utxoPool.lastKeyBefore(amount);
+    return utxoPool[key];
+  }
+
+  List<TransactionOutput> collectOutputs(BigInt amount) {
+    // Create an intermediate pool which is commit on success
+    var pool = utxoPool;
+
+    var utxos = [];
+    var remainingAmount = amount;
+
+    while (true) {
+      // Check whether large enough UTXO exists
+      final aboveOutput = smallestAbove(remainingAmount);
+      if (aboveOutput != null) {
+        utxos.add(aboveOutput);
+        utxoPool = pool; // Commit
+        return utxos;
+      }
+
+      // Find largest UTXO below the remaining amount.
+      final belowOutput = largestBelow(remainingAmount);
+      if (belowOutput == null) {
+        // No UTXOs left
+        throw Exception('Insufficient balance');
+      }
+      utxos.add(belowOutput);
+      remainingAmount -= belowOutput.satoshis;
+    }
+  }
+}
