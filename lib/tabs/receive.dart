27d3fe2da8b23eaae4f17fbd44150546349f1d6d
Ademir Villena <limonadev@whiteleaf.dev>
2020-11-09 17:19:16 -0500

    Added copy button on receive screen (#42)
    Add copy button to receive screen so that users can copy their
    address and send it to others easily when not using QR codes.

--- lib/tabs/receive.dart
+++ lib/tabs/receive.dart
@@ -1,4 +1,5 @@
 import 'package:cashew/wallet/wallet.dart';
+import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:cashew/constants.dart';
@@ -19,27 +20,58 @@ class ReceiveTab extends StatelessWidget {
     final _controller = TextEditingController(text: strAddress);
 
     final manualCard = Card(
-        child: Column(children: [
-          ListTile(
-            title: const Text('Text Address'),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _controller,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Container(
+                  height: 60.0,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Clipboard.setData(
+                        ClipboardData(
+                          text: strAddress,
+                        ),
+                      );
+
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Copied address to Clipboard'),
+                          duration: Duration(seconds: 1),
+                        ),
+                      );
+                    },
+                    child: Icon(Icons.copy),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controller,
-                readOnly: true,
-                decoration: InputDecoration(border: OutlineInputBorder()),
-              ))
-        ]),
-        elevation: stdElevation);
+        ],
+      ),
+      elevation: stdElevation,
+    );
 
     final qrCard = Card(
-      child: Column(children: [
-        ListTile(
-          title: const Text('QR Address'),
-        ),
-        QrImage(data: strAddress, version: QrVersions.auto)
-      ]),
+      child: QrImage(
+        data: strAddress,
+        version: QrVersions.auto,
+      ),
       elevation: stdElevation,
     );
 
