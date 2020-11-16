d3fe1f8a727cce002d3fca822673fa47efb4f2f2
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-11-16 10:30:27 -0800

    Allow multiple electrum servers with one selected randomly (#49)
    As per title.

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MainPageState extends State<MainPage> {
           '',
           Wallet(
             'todo path',
-            ElectrumFactory(Uri.parse(electrumUrl)),
+            ElectrumFactory(electrumUrls),
           ),
         );
       },
