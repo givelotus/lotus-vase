90189a469aa65f5ade8c2e07f5139b6812001242
chinitadelrey <26187282+chinitadelrey@users.noreply.github.com>
2020-11-16 08:11:39 -0800

    Fix wallet initialization (#47)
    Somehow this line got removed in a previous PR. This is necessary for
    the GUI to finish loading.

--- lib/viewmodel.dart
+++ lib/viewmodel.dart
@@ -18,6 +18,7 @@ class CashewModel with ChangeNotifier {
         .initialize()
         .then((value) => this.activeWallet.updateUtxos())
         .then((value) => this.activeWallet.refreshBalanceLocal())
+        .then((value) => initialized = true)
         .then((value) => this.activeWallet.startUtxoListeners());
   }
 
