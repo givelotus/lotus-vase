2b1bfa4d9182e96bf40416124ff52da65d81598c
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-11-16 10:29:21 -0800

    Allow self-signed certificates for websocket connections (#48)
    As per title. Manually allow self-signed certificates

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const stdElevation = 6.0;
 const stdPadding = EdgeInsets.all(12.0);
-const electrumUrl = 'ws://51.79.81.158:50003';
+const electrumUrl = 'https://51.79.81.158:50004';
 
 const inputSize = 148;
 const outputSize = 34;
