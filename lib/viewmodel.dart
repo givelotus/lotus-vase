6f0596fd11cb273012d40679244ddd693d14c3f5
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-11-28 16:08:44 -0800

    Refactor wallet initialization and move save/restore to viewmodel (#77)
    This commit lifts the disk read/writes to the ViewModel (where it should
    be), and changes the initialization flow so that the whole app is
    reactive to changes on that model. Part of this is done by splitting out
    the Send screen model into it's own entity, so that changes to that do
    not cause a full app refresh. This way, only changes to the wallet/seed
    cause this behavior.
    
    I realize this change is rather huge, and probably should be swapped out
    for several peices. However, I don't have time to split it up, and it's=
    not immediately obvious how they could be split. There's essentially aborts
    few things going on:
    
    1. Lifitng up storage to the viewmodel.
    2. Making the balance reactive so that a loading screen is not necessary
       and fact that the wallet isn't initialized does not explode the app
       while it's initializing (without a loading screen);
    3. Splitting the WalletModel from the SendModel so that the WalletModel
       updates may be subscribed to. Changes to the seed will cause the
       entire app to refresh essentially.
    
    Co-authored-by: Harry Barber <harrybarber@protonmail.com>

--- lib/viewmodel.dart
+++ lib/viewmodel.dart
@@ -1,22 +1,65 @@
+import 'package:cashew/bitcoincash/bitcoincash.dart';
 import 'package:flutter/foundation.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'package:cashew/wallet/wallet.dart';
+import 'package:cashew/wallet/keys.dart';
+import 'package:cashew/wallet/storage/keys.dart';
+import 'package:cashew/constants.dart';
 
-class CashewModel with ChangeNotifier {
-  String _sendToAddress;
-  int _sendAmount;
+import 'package:cashew/bitcoincash/src/networks.dart';
 
-  Wallet _activeWallet;
+import 'electrum/client.dart';
+
+const SCHEMA_VERSION_KEY = 'schema_version';
+
+const CURRENT_SCHEMA_VERSION = '0.2.0';
+
+const STORAGE_SEED_KEY = 'seed';
+const STORAGE_XPUB_KEY = 'rootKey';
+
+/// Generate a fresh wallet.
+Future<Wallet> generateNewWallet(String seed,
+    {network = NetworkType.TEST}) async {
+  final keys = await Keys.construct(seed);
+  final electrumFactory = ElectrumFactory(electrumUrls);
+
+  return Wallet(keys, electrumFactory, network: network);
+}
+
+class WalletModel with ChangeNotifier {
+  Wallet _wallet;
   bool _initialized = false;
+  String _seed = '';
+  // Internally use a ValueNotifier here, as we don't want the entire wallet
+  // to refresh when this field is updated.
+  // We should probably introduce a secondary model of some sort.
+  final ValueNotifier<int> balance;
+  final FlutterSecureStorage _storage;
 
-  CashewModel(sendToAddress, activeWallet) {
-    this.sendToAddress = _sendToAddress;
-    this.activeWallet = activeWallet;
-    _sendAmount = null;
-    this
-        .activeWallet
-        .loadFromDisk()
-        .then((value) => this.activeWallet.initialize())
-        .then((value) => initialized = true);
+  // TODO: Storage should be injected
+  WalletModel()
+      : _storage = FlutterSecureStorage(),
+        balance = ValueNotifier<int>(null) {
+    initializeModel(); // Run in background.
+  }
+
+  Future<void> initializeModel() async {
+    final readSucceed = await readFromDisk();
+    if (!readSucceed) {
+      // final testSeed =
+      //     'festival shrimp feel before tackle pyramid immense banner fire wash steel fiscal';
+      final mnemonicGenerator = Mnemonic();
+      final seed = mnemonicGenerator.generateMnemonic();
+      _seed = seed;
+
+      // Don't notify listeners. Initlaize will do that
+      _wallet = await generateNewWallet(_seed);
+    }
+    wallet.balanceUpdateHandler = (balance) => this.balance.value = balance;
+    await wallet.initialize();
+    initialized = true;
   }
 
   set initialized(bool newValue) {
@@ -24,26 +67,116 @@ class CashewModel with ChangeNotifier {
     notifyListeners();
   }
 
+  String get seed => _seed;
+
+  set seed(String newValue) {
+    _seed = newValue;
+    balance.value = null;
+    _initialized = false;
+    notifyListeners();
+    generateNewWallet(_seed).then((newWallet) {
+      _wallet = newWallet;
+      wallet.balanceUpdateHandler = (balance) => this.balance.value = balance;
+      wallet.initialize();
+      initialized = true;
+    });
+  }
+
   bool get initialized => _initialized;
 
-  set sendAmount(int newValue) {
-    _sendAmount = newValue;
+  Wallet get wallet => _wallet;
+
+  set wallet(Wallet newValue) {
+    _wallet = newValue;
     notifyListeners();
   }
 
-  int get sendAmount => _sendAmount;
-
-  Wallet get activeWallet => _activeWallet;
-
-  set activeWallet(Wallet newValue) {
-    _activeWallet = newValue;
-    notifyListeners();
+  Future<String> readSchemaVersion() {
+    final storage = FlutterSecureStorage();
+    return storage.read(key: SCHEMA_VERSION_KEY);
   }
 
-  String get sendToAddress => _sendToAddress;
+  Future<void> writeSchemaVersion() {
+    final storage = FlutterSecureStorage();
+    return storage.write(
+        key: SCHEMA_VERSION_KEY, value: CURRENT_SCHEMA_VERSION);
+  }
 
-  set sendToAddress(String newValue) {
-    _sendToAddress = newValue;
-    notifyListeners();
+  Future<void> writeToDisk() async {
+    // Persist schema version
+    await writeSchemaVersion();
+
+    // Persist keys
+    // TODO: keys.keys is silly naming
+    await writeKeysToDisk(wallet.keys.keys);
+  }
+
+  Future<bool> readFromDisk() async {
+    // Persist schema version
+    final schemaVersion = await readSchemaVersion();
+    if (schemaVersion != CURRENT_SCHEMA_VERSION) {
+      return false;
+    }
+
+    // Persist keys
+    final keys = await readKeysFromDisk();
+
+    wallet = Wallet(
+      keys,
+      ElectrumFactory(electrumUrls),
+      network: network,
+    );
+
+    return true;
+  }
+
+  Future<void> writeKeysToDisk(List<KeyInfo> keys) async {
+    // Write private keys
+    final keyWrites = keys.asMap().entries.map((entry) {
+      final index = entry.key;
+      final keyInfo = entry.value;
+      final storedKey = StoredKey.fromKeyInfo(keyInfo);
+      return storedKey.writeToDisk(index);
+    });
+    await Future.wait(keyWrites);
+    // Persist seed
+    await _storage.write(
+      key: STORAGE_SEED_KEY,
+      value: seed,
+    );
+
+    // Persist XPub
+    await _storage.write(
+      key: STORAGE_XPUB_KEY,
+      value: wallet.keys.rootKey.toString(),
+    );
+
+    // Write metadata
+    final metadata = KeyStorageMetadata(keys.length);
+    await metadata.writeToDisk();
+  }
+
+  Future<Keys> readKeysFromDisk() async {
+    // Read metadata
+    final metadata = await KeyStorageMetadata.readFromDisk();
+    final keyCount = metadata.keyCount;
+
+    // Read private keys
+    final storedKeyFutures = List<Future<StoredKey>>.generate(
+        keyCount, (index) => StoredKey.readFromDisk(index));
+    final storedKeys = await Future.wait(storedKeyFutures);
+
+    // Read seed
+    final seed = await _storage.read(key: STORAGE_SEED_KEY);
+
+    // Read root key to avoid regenerating from seed
+    final xpubHex = await _storage.read(key: STORAGE_XPUB_KEY);
+    final rootKey = HDPrivateKey.fromXpriv(xpubHex);
+
+    // Convert to key info
+    final keyInfo =
+        storedKeys.map((storedKey) => storedKey.toKeyInfo(network)).toList();
+
+    return Keys(seed, rootKey, keyInfo, network: network);
   }
 }
