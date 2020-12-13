000200b1288c0cbb94a1ac9e315ef8f30bcdd54c
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-12-13 11:33:29 -0800

    Remove bitcoin stack handler from codebase, was used by interpreter
    Interpreter has been removed, thus we do not need the stack class
    either.

--- lib/bitcoincash/bitcoincash.dart
+++ lib/bitcoincash/bitcoincash.dart
@@ -18,7 +18,6 @@ export 'src/block/blockheader.dart';
 export 'src/block/merkleblock.dart';
 export 'src/script/bchscript.dart';
 export 'src/script/opcodes.dart';
-export 'src/script/stack.dart';
 export 'src/script/scriptflags.dart';
 export 'src/transaction/transaction.dart';
 export 'src/transaction/transaction_output.dart';
