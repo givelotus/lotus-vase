50a709221473755ea29e83369078c4e626c75849
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-12-20 14:07:18 -0800

    Redesign SendInfo screen to have a better input method for amounts (#98)
    Implement a "calculator keyboard" and use it on the send information
    data entry screen instead of a standard input box. Additionally, we
    also get rid of the text entry field for an address and instead only
    allow users to paste into it. There are several other misc things thrown
    in with this redesign as well (e.g validation output that disables
    the send button).
    
    Co-authored-by: Ann-jie Ching <26187282+chinitadelrey@users.noreply.github.com>

---
+++ lib/components/calculator_keyboard/keyboard.dart
@@ -0,0 +1,70 @@
+library custom_keyboard;
+
+import 'package:flutter/widgets.dart';
+
+import 'button_definitions.dart';
+import 'calculator_button.dart';
+import 'calculator.dart';
+
+class CalculatorData {
+  int amount;
+  String function;
+  CalculatorData({this.amount, this.function});
+}
+
+class CalculatorKeyboard extends StatelessWidget {
+  final ValueNotifier<CalculatorData> dataNotifier;
+  final List<String> stack = [];
+
+  CalculatorKeyboard(
+      {Key key, String initialValue = '', @required this.dataNotifier})
+      : super(key: key) {
+    stack.addAll(initialValue.split(''));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var amount = 0;
+
+    final updateStack = () {
+      final evaluatedStack = stack.sublist(0);
+      evaluateExpression(evaluatedStack);
+      final takenNumbers = takeNumbers(evaluatedStack.reversed.toList());
+      amount = takenNumbers.truncate();
+
+      // This will be mutated, that's why we need to update the value.
+      dataNotifier.value =
+          CalculatorData(function: stack.join(''), amount: amount);
+    };
+
+    // FIXME: This seems like a terrible way to keep everything in sync on paste.
+    dataNotifier.addListener(() {
+      if (dataNotifier.value.amount == amount) {
+        return;
+      }
+      amount = dataNotifier.value.amount;
+      stack.clear();
+      stack.addAll(dataNotifier.value.function.split(''));
+    });
+
+    return Container(
+        child: Column(
+      children: [
+        Column(
+          children:
+              CalculatorRows.map((List<CalculatorItem> calculatorRowButtons) {
+            return Row(
+              children: calculatorRowButtons.map((CalculatorItem item) {
+                return CalculatorButton(
+                  button: item,
+                  stack: stack,
+                  onPressed: updateStack,
+                );
+              }).toList(),
+            );
+          }).toList(),
+        )
+      ],
+    ));
+  }
+}
