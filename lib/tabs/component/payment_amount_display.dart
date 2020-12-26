0cdff9b98bc096d04cef2e5f57cb382eaa04a1c4
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-12-25 23:34:13 -0800

    Use CalculatorKeyboard on receive screen for requesting funds (#125)
    As per title. Factor out PaymentAmountDisplay so it can also be used.
    This commit is pretty rough, and uses `ValueListenableBuilder` likely
    in too many places.

---
+++ lib/tabs/component/payment_amount_display.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class PaymentAmountDisplay extends StatelessWidget {
+  final String amount;
+  final String function;
+  PaymentAmountDisplay({Key key, this.amount, this.function})
+      : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        Expanded(
+            child: Text(
+          '${function}',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ))
+      ]),
+      Row(children: [
+        Expanded(
+            child: Text(
+          '= ${amount} sats',
+          textAlign: TextAlign.right,
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ))
+      ]),
+    ]);
+  }
+}
--- lib/tabs/receive.dart
+++ lib/tabs/receive.dart
@@ -2,12 +2,19 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:cashew/constants.dart';
 
+import 'package:cashew/constants.dart';
+import 'package:cashew/components/calculator_keyboard/keyboard.dart';
 import '../viewmodel.dart';
+import 'component/payment_amount_display.dart';
 
 class ReceiveTab extends StatelessWidget {
-  ReceiveTab({Key key}) : super(key: key);
+  final ValueNotifier<CalculatorData> keyboardNotifier;
+
+  ReceiveTab({Key key})
+      : keyboardNotifier = ValueNotifier<CalculatorData>(
+            CalculatorData(amount: 0, function: '')),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,58 +31,79 @@ class ReceiveTab extends StatelessWidget {
     final strAddress = keyInfo.address.toCashAddress();
     final _controller = TextEditingController(text: strAddress);
 
+    final createAddressUri = (CalculatorData data) {
+      if (data.amount == 0) {
+        return strAddress;
+      }
+      // Can't mutate the URI, so need a way to add query strings.
+      final parsedAddress = Uri.parse(strAddress);
+      return Uri(
+          scheme: parsedAddress.scheme,
+          path: parsedAddress.path,
+          queryParameters: {'amount': data.amount.toString()}).toString();
+    };
+
+    keyboardNotifier.addListener(() {
+      _controller.text = createAddressUri(keyboardNotifier.value);
+    });
+
     final manualCard = Card(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: _controller,
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+      child: ValueListenableBuilder(
+          valueListenable: keyboardNotifier,
+          builder: (context, CalculatorData balance, child) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                        controller: _controller,
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 16.0),
-                child: Container(
-                  height: 60.0,
-                  child: OutlinedButton(
-                    onPressed: () {
-                      Clipboard.setData(
-                        ClipboardData(
-                          text: strAddress,
-                        ),
-                      );
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: Container(
+                      height: 60.0,
+                      child: OutlinedButton(
+                        onPressed: () {
+                          Clipboard.setData(
+                            ClipboardData(
+                              text: createAddressUri(balance),
+                            ),
+                          );
 
-                      Scaffold.of(context).showSnackBar(
-                        copiedAd,
-                      );
-                    },
-                    child: Icon(Icons.copy),
+                          Scaffold.of(context).showSnackBar(
+                            copiedAd,
+                          );
+                        },
+                        child: Icon(Icons.copy),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+                ],
+              )),
       elevation: stdElevation,
     );
 
     final qrCard = Card(
-      child: QrImage(
-        data: strAddress,
-        version: QrVersions.auto,
-      ),
+      child: ValueListenableBuilder(
+          valueListenable: keyboardNotifier,
+          builder: (context, CalculatorData balance, child) => QrImage(
+                data: createAddressUri(balance),
+                version: QrVersions.auto,
+              )),
       elevation: stdElevation,
     );
+
+    final calculatorKeyboard =
+        CalculatorKeyboard(dataNotifier: keyboardNotifier, initialValue: '');
+    // Confirm Amount button widget writes to global SendModel
+    // and then switches to Slide to send button:
 
     return SafeArea(
       child: Column(
@@ -83,11 +111,18 @@ class ReceiveTab extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-            'Your Bitcoin Cash Address',
+            'Receive Funds',
             style: Theme.of(context).textTheme.headline6,
           ),
-          qrCard,
+          Expanded(child: qrCard),
           manualCard,
+          ValueListenableBuilder(
+              valueListenable: keyboardNotifier,
+              builder: (context, CalculatorData balance, child) =>
+                  PaymentAmountDisplay(
+                      amount: balance.amount.toString(),
+                      function: balance.function)),
+          calculatorKeyboard,
         ],
       ),
     );
