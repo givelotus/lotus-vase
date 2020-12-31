f0cf29d877872750c20c90487c1ee6bce4086df3
Shammah Chancellor <shammah.chancellor@gmail.com>
2020-12-31 14:13:27 -0800

    Fix send URI parsing to be consistent across app (#150)
    Make the QR code scan, and the pasting function, consistent in
    parsing URIs.

---
+++ lib/bitcoincash/utils/parse_uri.dart
@@ -0,0 +1,30 @@
+import 'package:cashew/bitcoincash/address.dart';
+
+class ParseURIResult {
+  String address;
+  int amount;
+
+  ParseURIResult({this.address, this.amount});
+}
+
+ParseURIResult parseSendURI(String uri) {
+  final parseObject = Uri.parse(uri);
+  final unparsedAmount = parseObject.queryParameters['amount'];
+  final amount = unparsedAmount == null
+      ? double.nan
+      : double.parse(unparsedAmount, (value) => double.nan);
+
+  Address(parseObject.path);
+
+  if (amount.isNaN) {
+    ParseURIResult(address: parseObject.path ?? '', amount: null);
+  }
+  int intAmount;
+  if (amount.truncateToDouble() != amount) {
+    intAmount = (amount * 100000000).truncate();
+  } else {
+    intAmount = amount.truncate();
+  }
+
+  return ParseURIResult(address: parseObject.path ?? '', amount: intAmount);
+}
