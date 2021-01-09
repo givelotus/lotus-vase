978936b563a84254eee876fe5fee4407fd9e8e2f
limonadev <limonadev@gmail.com>
2021-01-09 13:32:29 -0500

    Added encoding utils varIntWrite method tests

--- test/bitcoincash_test/encoding_test/encoding_utils_test.dart
+++ test/bitcoincash_test/encoding_test/encoding_utils_test.dart
@@ -33,4 +33,36 @@ void main() {
       );
     }
   });
+
+  test('varIntWriter method', () {
+    final toEncode = [
+      '01',
+      'ab12',
+      'ab12cd34',
+      '100000001',
+      '7fffffffffffffff',
+    ];
+    final expected = <Uint8List>[
+      Uint8List.fromList([1]),
+      Uint8List.fromList([0xfd, 18, 171]),
+      Uint8List.fromList([0xfe, 52, 205, 18, 171]),
+      Uint8List.fromList([0xff, 1, 0, 0, 0, 1, 0, 0, 0]),
+      Uint8List.fromList(
+        [0xff, 255, 255, 255, 255, 255, 255, 255, 127],
+      )
+    ];
+
+    for (var i = 0; i < toEncode.length; i++) {
+      final val = int.parse(
+        toEncode[i],
+        radix: 16,
+      );
+      final result = varIntWriter(val);
+
+      expect(
+        result,
+        equals(expected[i]),
+      );
+    }
+  });
 }
