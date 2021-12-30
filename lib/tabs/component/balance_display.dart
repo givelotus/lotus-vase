939fd296ab16da09ea691b3c2e6fb6e09caff6b8
Mags <4146037+MagsMagnoli@users.noreply.github.com>
2021-12-30 15:50:56 -0500

    fix balance overflow and alignment

--- lib/tabs/component/balance_display.dart
+++ lib/tabs/component/balance_display.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import '../../wallet/wallet.dart';
-import '../../constants.dart';
+
 import '../../../lotus/utils/format_amount.dart';
+import '../../constants.dart';
+import '../../wallet/wallet.dart';
 
 class BalanceDisplay extends StatelessWidget {
   final ValueNotifier<WalletBalance?> balanceNotifier;
@@ -19,41 +18,36 @@ class BalanceDisplay extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
           color: Colors.grey[400]!.withOpacity(0.6),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ValueListenableBuilder<WalletBalance?>(
-                  valueListenable: balanceNotifier,
-                  builder: (context, balance, child) {
-                    if (balance != null && balance.error != null) {
-                      return Text(
-                        balance.error.toString(),
-                        style: TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 13),
-                      );
-                    }
-                    if (balance == null || balance.balance == null) {
-                      return Text(
-                        'Loading...',
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 13),
-                      );
-                    }
-                    return Text.rich(TextSpan(
-                      text: '${formatAmount(balance.balance)}',
-                      style: TextStyle(
-                        color: Colors.black,
+          child: ValueListenableBuilder<WalletBalance?>(
+              valueListenable: balanceNotifier,
+              builder: (context, balance, child) {
+                if (balance != null && balance.error != null) {
+                  return Text(
+                    balance.error.toString(),
+                    style: TextStyle(
+                        color: Colors.red,
                         fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                      ),
-                    ));
-                  }),
-            ],
-          ),
+                        fontSize: 13),
+                  );
+                }
+                if (balance == null || balance.balance == null) {
+                  return Text(
+                    'Loading...',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13),
+                  );
+                }
+                return Text.rich(TextSpan(
+                  text: '${formatAmount(balance.balance)}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                  ),
+                ));
+              }),
         ),
       ),
     );
